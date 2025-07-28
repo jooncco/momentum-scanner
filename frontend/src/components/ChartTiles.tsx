3fe99c78f8823a23ef79770b8b872fa2d7e5a156
jooncco <jooncco.g@gmail.com>
2025-07-28 21:53:03 +0900

    [frontend] ChartTiles: change timezone to UTC+9

--- frontend/src/components/ChartTiles.tsx
+++ frontend/src/components/ChartTiles.tsx
@@ -17,7 +17,7 @@ export function ChartTiles({ ticker }: ChartTilesProps) {
       {timeframes.map((tf) => (
         <div key={tf.label} className="cyberTile">
           <iframe
-            src={`https://s.tradingview.com/widgetembed/?frameElementId=tradingview_${ticker}_${tf.label}&symbol=${ticker}&interval=${tf.interval}&hidesidetoolbar=0&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme=dark&style=1&timezone=Etc%2FUTC&enable_publishing=0&withdateranges=1&hide_top_toolbar=0&hide_legend=0&show_popup_button=1&popup_width=1000&popup_height=650&referral_id=12345`}
+            src={`https://s.tradingview.com/widgetembed/?frameElementId=tradingview_${ticker}_${tf.label}&symbol=${ticker}&interval=${tf.interval}&hidesidetoolbar=0&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme=dark&style=1&timezone=Asia%2FSeoul&enable_publishing=0&withdateranges=1&hide_top_toolbar=0&hide_legend=0&show_popup_button=1&popup_width=1000&popup_height=650&referral_id=12345`}
             width="100%"
             height="100%"
             frameBorder="0"
