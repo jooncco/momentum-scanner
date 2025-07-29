bb20f9b01147de1aa983c676725a8507c39e5baf
jooncco <jooncco.g@gmail.com>
2025-07-29 22:40:31 +0900

    :lipstick: style: chart border

--- frontend/src/components/ChartTiles.tsx
+++ frontend/src/components/ChartTiles.tsx
@@ -20,12 +20,9 @@ export function ChartTiles({ ticker }: ChartTilesProps) {
             src={`https://s.tradingview.com/widgetembed/?frameElementId=tradingview_${ticker}_${tf.label}&symbol=${ticker}&interval=${tf.interval}&hidesidetoolbar=0&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=[]&theme=dark&style=1&timezone=Asia%2FSeoul&enable_publishing=0&withdateranges=1&hide_top_toolbar=0&hide_legend=0&show_popup_button=1&popup_width=1000&popup_height=650&referral_id=12345`}
             width="100%"
             height="100%"
-            frameBorder="0"
-            allowTransparency={true}
-            scrolling="no"
             allowFullScreen
             title={`${ticker} ${tf.label} Chart`}
-            style={{ pointerEvents: "auto" }}
+            style={{ pointerEvents: "auto", border: "none" }}
           />
         </div>
       ))}
